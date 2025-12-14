b84637dfef42c8c8c2080d7179417bfb70a5b554
Ferretelic <kyleshouki@gmail.com>
2025-12-13 19:45:55 -0800

    config class

--- utils.py
+++ utils.py
@@ -1,22 +1,30 @@
 import gzip
 import json
 
-min_rating = 4
-country = "Japanese"
+class Config():
+    def __init__(self, proj_path):
+        self.min_rating = 4
+        self.country = "Japanese"
 
-raw_path = "../dataset/raw"
-processed_path = "../dataset/processed"
+        self.raw_path = f"{proj_path}/dataset/raw"
+        self.processed_path = f"{proj_path}/dataset/processed"
+        self.result_path = f"{proj_path}/results"
 
-meta_keys = ["gmap_id", "name", "address", "latitude", "longitude", "category", "avg_rating", "num_of_reviews", "price", "hours"]
-review_keys = ["gmap_id", "user_id", "name", "time", "rating"]
+        self.meta_keys = [
+            "gmap_id", "name", "address",
+            "latitude", "longitude", "category",
+            "avg_rating", "num_of_reviews", "price", "hours"
+        ]
+
+        self.review_keys = ["gmap_id", "user_id", "name", "time", "rating"]
 
 def parse(path):
     g = gzip.open(path, "r")
     for l in g:
         yield json.loads(l)
 
-def load_states():
-    with open(f"{raw_path}/states.txt", "r") as f:
+def load_states(config):
+    with open(f"{config.raw_path}/states.txt", "r") as f:
         states = f.read().split("\n")
 
     states = [state.replace(" ", "_") for state in states]
