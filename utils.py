d25074a105abfc36c5e1724559fe3d72bf8c5c87
Ferretelic <kyleshouki@gmail.com>
2025-12-14 23:14:07 +0000

    add evaluation in main

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import os
 import gzip
 import json
 
+import numpy as np
+
 def parse(path):
     g = gzip.open(path, "r")
     for l in g:
@@ -20,3 +22,18 @@ def check_process_complete(cfg):
     num_meta_files = len(os.listdir(cfg.paths.meta))
 
     return (num_states == num_review_files) and (num_states == num_meta_files)
+
+def check_training_started(cfg):
+    return os.path.exists(f"{cfg.paths.logs}/{cfg.name}.json")
+
+def check_training_completed(cfg):
+    with open(f"{cfg.paths.logs}/{cfg.name}.json", "r") as f:
+        logs = json.load(f)
+
+    early_stopping = cfg.training.early_stopping
+    ndcgs = np.array(logs["valid"]["ndcg"])
+    n_patience = ndcgs.shape[0] - (np.argmax(ndcgs) + 1)
+
+    if early_stopping == n_patience:
+        print("        Training has been already completed.")
+        return True
