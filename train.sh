c1969a8634af1e6feb0b902cae2d6bb088f7057c
Ferretelic <kyleshouki@gmail.com>
2025-12-14 11:52:43 +0000

    update all configs

--- train.sh
+++ train.sh
@@ -1,2 +1,9 @@
+python main.py --config-name=batch_4096
+python main.py --config-name=batch_2048
+python main.py --config-name=batch_1024
+
+python main.py --config-name=reg_0.001
+python main.py --config-name=reg_0.00001
+
 python main.py --config-name=lr_0.01
 python main.py --config-name=lr_0.001
