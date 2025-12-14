450ff6b80385377444306eec828f3f2909777602
Ferretelic <kyleshouki@gmail.com>
2025-12-13 21:39:38 -0800

    add shortcut variable for paths

--- processing/combine_file.py
+++ processing/combine_file.py
@@ -5,7 +5,7 @@ import pandas as pd
 from utils import *
 
 def combine_state_files(cfg, mode):
-    file_path = f"{cfg.paths.processed}/{cfg.dataset.country}/{mode}.csv"
+    file_path = f"{cfg.paths.country}/{mode}.csv"
 
     if os.path.exists(file_path):
         return
@@ -14,7 +14,7 @@ def combine_state_files(cfg, mode):
 
     all_data = []
     for state in states:
-        data = pd.read_csv(f"{cfg.paths.processed}/{cfg.dataset.country}/{mode}/{state}.csv")
+        data = pd.read_csv(f"{cfg.paths.country}/{mode}/{state}.csv")
         data["state"] = data["gmap_id"].apply(lambda x: state)
         all_data.append(data)
 
