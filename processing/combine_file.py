f7d9a6fb5bc11b2f7e968a329b61b1265186c2b2
Ferretelic <kyleshouki@gmail.com>
2025-12-13 16:27:13 -0800

    combine files

---
+++ processing/combine_file.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+from utils import *
+
+def combine_state_files(mode):
+    states = load_states()
+
+    all_data = []
+    for state in states:
+        data = pd.read_csv(f"{processed_path}/{country}/{mode}/{state}.csv")
+        data["state"] = data["gmap_id"].apply(lambda x: state)
+        all_data.append(data)
+
+    df = pd.concat(all_data, axis=0)
+    df.to_csv(f"{processed_path}/{country}/{mode}.csv", index=False)
+
+if __name__ == "__main__":
+    combine_state_files("meta")
+    combine_state_files("review")
