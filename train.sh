644b97226d6d4c117e296e1da57ceea639d536ed
Ferretelic <kyleshouki@gmail.com>
2025-12-14 14:08:35 +0000

    add logging config

--- train.sh
+++ train.sh
@@ -3,15 +3,16 @@ python main.py --config-name=emb_1024 > /dev/null 2>&1 &
 
 python main.py --config-name=emb_256_n_layers_4 > /dev/null 2>&1 &
 python main.py --config-name=emb_512_n_layers_4 > /dev/null 2>&1 &
-python main.py --config-name=emb_1024_n_layers_4 > /dev/null 2>&1 &
+# python main.py --config-name=emb_1024_n_layers_4 > /dev/null 2>&1 &
 
 python main.py --config-name=emb_256_reg_0.001 > /dev/null 2>&1 &
 python main.py --config-name=emb_512_reg_0.001 > /dev/null 2>&1 &
-python main.py --config-name=emb_1024_reg_0.001 > /dev/null 2>&1 &
+# python main.py --config-name=emb_1024_reg_0.001 > /dev/null 2>&1 &
 
 python main.py --config-name=reg_0.01 > /dev/null 2>&1 &
 python main.py --config-name=reg_0.001 > /dev/null 2>&1 &
 
+python main.py --config-name=lr_0.0001 > /dev/null 2>&1 &
 
 wait
 echo "Finished training all the models"
