bd6d4be5033bb53c28b780b2cd6f1ffa9b771be6
Ferretelic <kyleshouki@gmail.com>
2025-12-15 01:06:22 +0000

    split embeddings into chunks

---
+++ training/test.py
@@ -0,0 +1,7 @@
+
+def test_model_performance(cfg):
+    embedding_path = f"{cfg.paths.emb}"
+    if not
+embeddings = self.get_embeddings()
+        recall, ndcgs = self.evaluator.evaluate(embeddings)
+        return recall, ndcgs
