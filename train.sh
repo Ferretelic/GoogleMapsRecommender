17d2d39b39062d597592d2530d523b156cf98c58
Ferretelic <kyleshouki@gmail.com>
2025-12-15 08:59:49 +0000

    experiment with n_layers 8

--- train.sh
+++ train.sh
@@ -1,8 +1,12 @@
-python main.py --config-name=emb_512_n_layers_6_reg_0.001_gentle_0.1 > /dev/null 2>&1 &
-python main.py --config-name=emb_512_n_layers_6_reg_0.001_gentle_0.05 > /dev/null 2>&1 &
+# python main.py --config-name=emb_512_n_layers_5_reg_0.001_gentle_0.1 > /dev/null 2>&1 &
+# python main.py --config-name=emb_512_n_layers_5_reg_0.001_gentle_0.05 > /dev/null 2>&1 &
 
-python main.py --config-name=emb_512_n_layers_7_reg_0.001_gentle_0.1 > /dev/null 2>&1 &
-# python main.py --config-name=emb_512_n_layers_7_reg_0.001_gentle_0.05 > /dev/null 2>&1 &
+python main.py --config-name=emb_512_n_layers_7_reg_0.001_gentle_0.05 > /dev/null 2>&1 &
+
+# python main.py --config-name=emb_512_n_layers_8  > /dev/null 2>&1 &
+python main.py --config-name=emb_512_n_layers_8_reg_0.001_gentle_0.1 > /dev/null 2>&1 &
+python main.py --config-name=emb_512_n_layers_8_reg_0.001_gentle_0.05 > /dev/null 2>&1 &
+python main.py --config-name=emb_512_n_layers_8_reg_0.001 > /dev/null 2>&1 &
 
 wait
 echo "Finished training all the models"
