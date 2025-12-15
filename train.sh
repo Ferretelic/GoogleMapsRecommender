6d47c3fd9d6af95d3ce85111685ca34467560223
Ferretelic <kyleshouki@gmail.com>
2025-12-15 00:15:08 +0000

    experiment with n_layers 6

--- train.sh
+++ train.sh
@@ -1,10 +1,5 @@
-python main.py --config-name=emb_1024_n_layers_4 > /dev/null 2>&1 &
-python main.py --config-name=emb_1024_reg_0.001 > /dev/null 2>&1 &
-
-python main.py --config-name=emb_512_n_layers_4_reg_0.001 > /dev/null 2>&1 &
-python main.py --config-name=emb_512_n_layers_5_reg_0.001 > /dev/null 2>&1 &
-
-python main.py --config-name=n_layers_5 > /dev/null 2>&1 &
+python main.py --config-name=emb_512_n_layers_6_reg_0.001 > /dev/null 2>&1 &
+python main.py --config-name=n_layers_6 > /dev/null 2>&1 &
 
 wait
 echo "Finished training all the models"
