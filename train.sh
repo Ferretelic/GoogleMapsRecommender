dd96b54750f05c9c4d1595d68952f929d064d854
Ferretelic <kyleshouki@gmail.com>
2025-12-16 02:01:45 +0000

    add new pip libraries

--- train.sh
+++ train.sh
@@ -1,12 +1,7 @@
-# python main.py --config-name=emb_512_n_layers_5_reg_0.001_gentle_0.1 > /dev/null 2>&1 &
-# python main.py --config-name=emb_512_n_layers_5_reg_0.001_gentle_0.05 > /dev/null 2>&1 &
+python main.py --config-name=n_layers_8  > /dev/null 2>&1 &
+wait
 
-python main.py --config-name=emb_512_n_layers_7_reg_0.001_gentle_0.05 > /dev/null 2>&1 &
-
-# python main.py --config-name=emb_512_n_layers_8  > /dev/null 2>&1 &
-python main.py --config-name=emb_512_n_layers_8_reg_0.001_gentle_0.1 > /dev/null 2>&1 &
-python main.py --config-name=emb_512_n_layers_8_reg_0.001_gentle_0.05 > /dev/null 2>&1 &
-python main.py --config-name=emb_512_n_layers_8_reg_0.001 > /dev/null 2>&1 &
+python main.py --config-name=emb_512_n_layers_7_reg_0.01  > /dev/null 2>&1 &
 
 wait
 echo "Finished training all the models"
