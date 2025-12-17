ac09ffac966d1186aecea28d2ecdd18a1df8ced6
Ferretelic <kyleshouki@gmail.com>
2025-12-17 13:46:11 +0000

    experiment with larger geo threshold

--- train.sh
+++ train.sh
@@ -1,9 +1,15 @@
-python main.py --config-name=geo_distance_20.0_1.0 > /dev/null 2>&1 &
-python main.py --config-name=geo_distance_20.0_1.5 > /dev/null 2>&1 &
-python main.py --config-name=geo_distance_20.0_2.0 > /dev/null 2>&1 &
-python main.py --config-name=geo_distance_20.0_3.0 > /dev/null 2>&1 &
-python main.py --config-name=geo_distance_20.0_4.0 > /dev/null 2>&1 &
-python main.py --config-name=geo_distance_20.0_5.0 > /dev/null 2>&1 &
+python main.py --config-name=geo_distance_5 > /dev/null 2>&1 &
+python main.py --config-name=geo_distance_10 > /dev/null 2>&1 &
+python main.py --config-name=geo_distance_15 > /dev/null 2>&1 &
+python main.py --config-name=geo_distance_20 > /dev/null 2>&1 &
+
+wait
+
+python main.py --config-name=geo_distance_20.0_6.0 > /dev/null 2>&1 &
+python main.py --config-name=geo_distance_20.0_7.0 > /dev/null 2>&1 &
+python main.py --config-name=geo_distance_20.0_8.0 > /dev/null 2>&1 &
+
+python main.py --config-name=gcl > /dev/null 2>&1 &
 
 wait
 echo "Finished training all the models"
