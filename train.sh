1255ebc66434d4a65ad807675dbfb8de8bf6e49e
Ferretelic <kyleshouki@gmail.com>
2025-12-18 00:27:03 +0000

    SimGCL without layer decay

--- train.sh
+++ train.sh
@@ -1,6 +1,5 @@
-python main.py --config-name=emb_512_n_layers_6_reg_0.001_gentle_0.1_gcl_geo_distance_20.0_8.0
-python main.py --config-name=emb_512_n_layers_7_reg_0.001_gentle_0.1_gcl_geo_distance_20.0_8.0
-python main.py --config-name=emb_512_n_layers_8_reg_0.001_gentle_0.1_gcl_geo_distance_20.0_8.0
+python main.py --config-name=emb_512_n_layers_3_reg_0.001_gcl
+python main.py --config-name=emb_512_n_layers_4_reg_0.001_gcl
 
 
 wait
