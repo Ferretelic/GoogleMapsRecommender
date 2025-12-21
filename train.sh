1c65e1a7e66474e3e57f39fc6489aa7b8d49961c
Ferretelic <kyleshouki@gmail.com>
2025-12-21 19:01:53 +0000

    remove best

--- train.sh
+++ train.sh
@@ -1,5 +1,2 @@
-python main.py --config-name=baseline dataset=Asian
 python main.py --config-name=best dataset=Asian
-
-python main.py --config-name=baseline dataset=Cafe
 python main.py --config-name=best dataset=Cafe
