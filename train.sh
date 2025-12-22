8e8946a01d1cf004ed24d9ac14de35d7362de357
Ferretelic <kyleshouki@gmail.com>
2025-12-22 12:44:39 +0000

    add validation configs

--- train.sh
+++ train.sh
@@ -1,2 +1,11 @@
-python train.py --config-name=best dataset=Asian
-python train.py --config-name=best dataset=Cafe
+python train.py --config-name=mixgcf_0.2_32_0.1 dataset=Japanese
+python train.py --config-name=mixgcf_0.2_32_0.2 dataset=Japanese
+
+python train.py --config-name=mixgcf_0.4_32_0.1 dataset=Japanese
+python train.py --config-name=mixgcf_0.4_32_0.2 dataset=Japanese
+
+python train.py --config-name=mixgcf_0.6_32_0.1 dataset=Japanese
+python train.py --config-name=mixgcf_0.6_32_0.2 dataset=Japanese
+
+python train.py --config-name=mixgcf_0.4_64_0.1 dataset=Japanese
+python train.py --config-name=mixgcf_0.4_64_0.2 dataset=Japanese
