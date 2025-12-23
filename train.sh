ec79b748892cd7589ac9267f965f68297d96cd73
Ferretelic <kyleshouki@gmail.com>
2025-12-23 04:02:12 +0000

    search best hyperparameter for MixGCF

--- train.sh
+++ train.sh
@@ -1,11 +1,2 @@
-python train.py --config-name=mixgcf_0.2_32_0.1 dataset=Japanese
-python train.py --config-name=mixgcf_0.2_32_0.2 dataset=Japanese
-
-python train.py --config-name=mixgcf_0.4_32_0.1 dataset=Japanese
-python train.py --config-name=mixgcf_0.4_32_0.2 dataset=Japanese
-
-python train.py --config-name=mixgcf_0.6_32_0.1 dataset=Japanese
-python train.py --config-name=mixgcf_0.6_32_0.2 dataset=Japanese
-
-python train.py --config-name=mixgcf_0.4_64_0.1 dataset=Japanese
-python train.py --config-name=mixgcf_0.4_64_0.2 dataset=Japanese
+python train.py --config-name=best dataset=Japanese
+python train.py --config-name=baseline dataset=Japanese
